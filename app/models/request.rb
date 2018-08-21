137dba4036916f943a65a13babe4a21594da2758
Matthew Frazier <frazier3388@yahoo.com>
2018-08-21 15:01:02 +0900

    Working on Validations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,7 @@
 class Request < ApplicationRecord
-  belongs_to :booking
+  belongs_to :dog through: :dog_requests
+  belongs_to :user
+
+  validates :pickup_location, presence: true
+  validates :dropoff_location, presence: true
 end
