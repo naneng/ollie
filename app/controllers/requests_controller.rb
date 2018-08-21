c67ae141d5f259fa52eaf217ba8446662912b99b
Katrina Ariola <kat3ariola@gmail.com>
2018-08-21 15:09:20 +0900

    created controllers for uses dogs requests

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,20 @@
+class RequestsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
