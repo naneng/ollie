7f3e2953088fd160f3762240437fc6f732e0b414
Matthew Frazier <frazier3388@yahoo.com>
2018-08-22 15:44:44 +0900

    changed mobile dropdown

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,6 @@
 class RequestsController < ApplicationController
-
   before_action :set_user, only: [ :new, :create, :edit, :destroy ]
-  before_action :set_request
+  before_action :set_request, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @requests = policy_scope(Request)
