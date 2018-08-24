8486ab59b22dbf23afa7c0fc7b0cc8c749fc0ea7
George Walden <George.walden7@gmail.com>
2018-08-24 14:28:50 +0900

    Make Booking

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,4 @@ import "../plugins/flatpickr";
 import {searchFunction} from "../components/search";
 import {bindEventListener} from "../components/checkbox";
 
-console.log("made it");
 bindEventListener();
