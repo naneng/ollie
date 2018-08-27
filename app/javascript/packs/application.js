9e181727f6c1be667ed133a49a8524d2c6e2a3e4
George Walden <George.walden7@gmail.com>
2018-08-27 15:36:31 +0900

    Followers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,5 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import {searchFunction} from "../components/search";
 import {bindEventListener} from "../components/checkbox";
-import {followToggleFunction} from "../components/follow";
 
 bindEventListener();
