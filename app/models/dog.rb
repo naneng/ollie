9e01ef0b1b48dc4b490566bcc44c425b387022c1
Matthew Frazier <frazier3388@yahoo.com>
2018-08-27 16:50:56 +0900

    added button to view requests

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :dog_requests
+  has_many :requests, through: :dog_requests, dependent: :destroy
   validates :name, presence: true
 
 end
